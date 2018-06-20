42ca323f688e2910e5b4ad439b5a2d656e2cdfb2
Jonathan Hussey <jhusseydesign@gmail.com>
2018-06-20 07:28:19 +0100

    Added Objective enum

---
+++ objective/Objective.go
@@ -0,0 +1,16 @@
+package objective
+
+// Objective is a type which defines whether an optimiser is solving a
+// maximisation, minimisation or multiobjective problem
+type Objective string
+
+const (
+	// Maximisation is an objective that is used by an optimiser when trying to
+	// maximise the result of evaluating a chromosome. It is typically used by
+	// the optimiser to sort the chromosomes if there is a population of them
+	Maximisation Objective = "Maximisation"
+
+	// Minimisation defines a type of objective that aims to minimise the
+	// evaluation function.
+	Minimisation Objective = "Minimisation"
+)
